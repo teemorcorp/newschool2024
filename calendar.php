60bfacffaf526a26ea06b03ce760d5bb1b9bc074
TJ Moore <teemorcorp@gmail.com>
2023-09-13 18:07:11 -0700

    Adjusting calendar.

--- calendar.php
+++ calendar.php
@@ -27,9 +27,7 @@ testadmin();
         <p class="text-center"><span style="font-size: 32px;">Calendar</span></p>
                 
         <div class="row">
-            <div class="col-sm-2">
-                <div id="datepicker"></div> 
-            </div>
+            <div class="col-sm-2"></div>
             <div class="col-sm-8">
                 <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FPhoenix&showTitle=0&showPrint=0&showTabs=1&showCalendars=0&src=aWhuYmlibGVAZ21haWwuY29t&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
             </div>
@@ -37,7 +35,7 @@ testadmin();
         </div>
     </div>
 
-    <div id="boxed" style="margin-bottom: 50px;">&nbsp;</div>
+    <div id="boxed" style="margin-bottom: 50px;">&nbsp;Test:<div id="datepicker"></div> </div>
 </div>
 <?php
 include "tmp/footer.php";
