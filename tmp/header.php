2280e88b489012ae55639d294282b6d0f47a82d8
TJ Moore <teemorcorp@gmail.com>
2023-09-13 18:13:55 -0700

    Replaced static javascipt file links with CDN links. Same with the CSS links. Calendar widget now works.

--- tmp/header.php
+++ tmp/header.php
@@ -96,21 +96,24 @@ global $menuid, $goal, $current, $pct, $userid;
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
         <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" rel="stylesheet" />
-        <link href="css/styles.css" rel="stylesheet" />
 
         <!-- **********************  TintMCE  ****************************************************************** -->
         <script src="https://cdn.tiny.cloud/1/qa5ollct4qx4a9lkzbhdh1ki6763pwyi6jx3m0s5ut86u8by/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
         <!-- **********************  END TintMCE  ******************************************************************-->
 
         <!-- **********************  JQUERY  ********************************************************************** -->
-        <link href="css/jquery-ui-min.css" rel="stylesheet" />
-        <script src="js/jquery.js"></script>        
-        <script src="js/jquery-ui-min.js"></script>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
         <!-- **********************  END JQUERY  ******************************************************************-->
 
         <!-- ********************************************************************************************************
         *******  DATE PICKER / CALENDAR
         ***********************************************************************************************************-->
+
+
+        <link href="css/styles.css" rel="stylesheet" />
+        
         <script>
             $( function() {
                 $( "#datepicker" ).datepicker();
